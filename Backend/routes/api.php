e4c6c903a0b9db73d2d3f2d290c7eda85960c30d
Kiady Nirina <kiady142ram@gmail.com>
2024-11-04 19:55:45 +0300

    Backend : (api.php) edit routes

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\FriendController;
+
+/* Authentication */
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login'])->name('login');
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
