376c7247619e443e6b227552c86e6edf45f722a2
Kiady Nirina <kiady142ram@gmail.com>
2024-11-05 18:25:26 +0300

    Backend : (AuthController.php) add login function

--- Backend/app/Http/Controllers/AuthController.php
+++ Backend/app/Http/Controllers/AuthController.php
@@ -25,4 +25,21 @@ class AuthController extends Controller
 
         return response()->json($user, 201);
     }
+
+    public function login (Request $request) 
+    {
+        $request -> validate([
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string',
+        ]);
+
+        if (!Auth::attempt($request -> only('email', 'password'))) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user -> createToken('Personal Access Token');
+
+        return response()->json(['token' => $token, 'user' => $user]);
+    }
 }
