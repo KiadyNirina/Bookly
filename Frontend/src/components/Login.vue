8234961052e30bba26d4c1767935fe39c1dfddcd
Kiady Nirina <kiady142ram@gmail.com>
2024-11-06 12:51:52 +0300

    Frontend : add error login form

--- Frontend/src/components/Login.vue
+++ Frontend/src/components/Login.vue
@@ -7,10 +7,14 @@ export default {
             email: '',
             password: '',
             isPasswordVisible: false,
+            errorMess: '',
+            hasError: false,
+            isLoading: false,
         };
     },
     methods: {
         async login() {
+            this.isLoading = true;
             try {
                 const response = await api.login({
                     email: this.email,
@@ -20,7 +24,14 @@ export default {
                 this.$router.push('/dashboard');
             } catch (error) {
                 console.error(error);
-                alert('Login failed');
+                if (error.response && error.response.status === 401) {
+                    this.errorMess = "Email ou mot de passe incorrect.";
+                } else {
+                    this.errorMess = "Une erreur est survenue. Veuillez réessayer.";
+                }
+                this.hasError = true;
+            } finally {
+                this.isLoading = false;
             }
         }
     }
@@ -31,10 +42,14 @@ export default {
     <div class="content-login">
         <div class="form-login">
             <img src="../../public/giphy_book (4).gif" alt="">
-            <div class="sect2">
+            <div :class="['sect2', {'error-border': hasError}]">
                 <h1>Se connecter</h1>
-                <form action="">
-                    <input type="text" v-model="email" placeholder="Entrer votre email">
+                <p v-if="errorMess" id="error">{{ errorMess }}</p>
+                <form @submit.prevent="login" action="">
+                    <input 
+                        type="text" 
+                        v-model="email" 
+                        placeholder="Entrer votre email">
                     <input name=""
                         :type="isPasswordVisible ? 'text' : 'password'"
                         id="password"
@@ -48,7 +63,8 @@ export default {
                         Afficher le mot de passe
                     </label>
                     <a href="" id="passwordForgot">Mot de passe oublié?</a>
-                    <button>Se connecter</button>
+                    <button v-if="isLoading" disabled>loading...</button>
+                    <button v-else>Se connecter</button>
                 </form>
                 <p>Ou :</p>
                 <div class="other">
@@ -124,6 +140,13 @@ export default {
 .content-login .form-login .sect2 form input[type='checkbox']:checked{
     color: #E67E22;
 }
+.content-login .form-login .error-border form input[type='text'], .content-login .form-login .error-border form input[type='password']{
+    border: 1px solid rgb(255, 62, 62);
+}
+#error{
+    color: rgb(255, 62, 62);
+    font-size: 13px;
+}
 .content-login .form-login .sect2 form button{
     width: 100%;
     height: 40px;
@@ -140,6 +163,9 @@ export default {
     background-color: transparent;
     color: #E67E22;
     cursor: pointer;
+}
+.content-login .form-login .sect2 form button:disabled {
+    cursor: not-allowed;
 }
 .content-login .form-login .sect2 p{
     color: rgba(255, 255, 255, 0.659);
