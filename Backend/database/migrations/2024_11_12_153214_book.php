f01e7191f8ab879849b1734c66e0f264ee50ca5d
Kiady Nirina <kiady142ram@gmail.com>
2024-11-12 18:47:32 +0300

    Added Book model

---
+++ Backend/database/migrations/2024_11_12_153214_book.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table -> id();
+            $table -> string('title');
+            $table -> string('author');
+            $table -> text('description');
+            $table -> string('genre');
+            $table->unsignedBigInteger('posted_by');
+            $table -> string('lang', 5);
+            $table -> integer('page');
+            $table -> date('date');
+            $table -> string('picture');
+            $table -> timestamps();
+
+            $table -> foreign('posted_by') -> references('id') -> on('users') -> onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
