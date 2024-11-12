cc5bf41844b32d4ecc1d081e2767d9ffd21b1664
Kiady Nirina <kiady142ram@gmail.com>
2024-11-12 17:58:44 +0300

    Added UserController and new API route for user info

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -15,8 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 /* Authentication */
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::get('/user', [UserController::class, 'userInfo'])->middleware('auth:sanctum');
