b60c41f1b3972deb387ac4cfad78bfb41de79319
Kiady Nirina <kiady142ram@gmail.com>
2025-05-16 20:55:38 +0300

    feat: integrate Tailwind CSS into the frontend project

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue';
 import router from './router/router'; // Importation du routeur Vue
 //import store from './store'; // Importation du store (Vuex)
 import apiClient from './plugins/axios'; // Configuration globale de axios
-//import './assets/main.css';
+import './assets/global.css'; // Importation du fichier CSS principal
 
 const app = createApp(App);
 
