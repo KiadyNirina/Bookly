bf0c38802e4b86087988f6c909a70b8f405502eb
Kiady <kiady142ram@gmail.com>
2026-02-13 21:56:07 +0300

    Add Save model and migration for book-user relationships

---
+++ Backend/database/migrations/2026_02_13_184505_create_save_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('save', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('book');
+            $table->unsignedBigInteger('user');
+            $table->timestamps();
+
+            $table -> foreign('book') -> references('id') -> on('books') -> onDelete('cascade');
+            $table -> foreign('user') -> references('id') -> on('users') -> onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('save');
+    }
+};
